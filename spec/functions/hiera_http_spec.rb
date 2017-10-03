bd1538ea052abd932941090d14f6dc012889b49e
Craig Dunn <craig@craigdunn.org>
2017-10-03 13:03:47 +0200

    added more tests

--- spec/functions/hiera_http_spec.rb
+++ spec/functions/hiera_http_spec.rb
@@ -14,19 +14,19 @@ describe FakeFunction do
     allow(@context).to receive(:interpolate)
     allow(@context).to receive(:cache)
     allow(@context).to receive(:not_found)
+    allow(@context).to receive(:interpolate).with('/path').and_return('/path')
   end
 
   describe "#lookup_key" do
 
     context "Should run" do
       let(:options) { {
-        'uri' => 'http://192.168.0.1:8080/foo/bar'
+        'uri' => 'http://192.168.0.1:8080/path'
       } }
 
       it "should run" do
         expect(LookupHttp).to receive(:new).with({ :host => '192.168.0.1', :port => 8080 })
-        expect(@lookuphttp).to receive(:get_parsed).with('/foo/bar').and_return('value')
-        expect(@context).to receive(:interpolate).with('/foo/bar').and_return('/foo/bar')
+        expect(@lookuphttp).to receive(:get_parsed).with('/path').and_return('value')
         expect(function.lookup_key('bar', options, @context)).to eq('value')
       end
     end
@@ -41,6 +41,45 @@ describe FakeFunction do
         function.lookup_key('foo::bar::tango', options, @context)
       end
     end
+
+    context "When confine_to_keys is set" do
+      let(:options) { {
+        'uri' => 'http://localhost/path',
+        'confine_to_keys' => [ /^tango.*/ ],
+      } }
+
+      before(:each) do
+        allow(@context).to receive(:interpolate).with('/path').and_return('/path')
+      end
+
+      it "should return not found for non matching keys" do
+        expect(@context).to receive(:not_found)
+        expect(@lookuphttp).not_to receive(:get_parsed)
+        function.lookup_key('bar', options, @context)
+      end
+
+      it "should run if key matches" do
+        expect(@context).not_to receive(:not_found)
+        expect(@lookuphttp).to receive(:get_parsed).with('/path').and_return('value')
+        expect(function.lookup_key('tangodelta', options, @context)).to eq('value')
+      end
+    end
+
+    context "Output of lookup_http" do
+      let(:options) { {
+        'uri' => 'http://localhost/path',
+      } }
+      it "should dig if the value is a hash" do
+        expect(@lookuphttp).to receive(:get_parsed).with('/path').and_return({ 'tango' => 'delta' })
+        expect(function.lookup_key('tango', options, @context)).to eq('delta')
+      end
+      it "should return the whole value if the value is a string" do
+        expect(@lookuphttp).to receive(:get_parsed).with('/path').and_return('delta')
+        expect(function.lookup_key('tango', options, @context)).to eq('delta')
+      end
+    end
+
+
       
       
   end
